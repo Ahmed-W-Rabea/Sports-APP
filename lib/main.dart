f8aa37dc9da00f080a367fb88cab3e2cf3b4ec76
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2023-09-02 15:43:46 +0300

    `last`

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/Splash_Screen.dart';
 import 'package:flutter_application_1/share/local/cache/cache_helper.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
